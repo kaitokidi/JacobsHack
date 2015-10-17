794dbdebbb45083f02f260644653d808ff9fd7bb
Pinkii- <pinkiibiza@gmail.com>
2015-10-17 20:06:51 +0200

    ini rotation

--- SceneRace.cpp
+++ SceneRace.cpp
@@ -3,10 +3,13 @@
 
 SceneRace::SceneRace(Game* g, sf::RenderWindow* w) : Scene(g,w) {
 	_nPlayers = 4;
+	_rotation = 0;
 	_players = std::vector<Player>(_nPlayers);
 	sf::Vector2u targetResolution(640,320);
 	_background = new Background(targetResolution);
 	initView(sf::Vector2i(targetResolution));
+
+
 	_rect.setSize(sf::Vector2f(targetResolution.x, targetResolution.y/4.0));
 	_rect.setPosition(sf::Vector2f(0,targetResolution.y*3.0/4.0));
 	_rect.setFillColor(sf::Color::Yellow);
@@ -34,7 +37,7 @@ void SceneRace::processInput() {
 
 void SceneRace::update(float deltaTime) {
 	for (int i = 0; i < _nPlayers; ++i) _players[i].update(deltaTime);
-	
+
 }
 	
 void SceneRace::render(sf::RenderTarget* target) {
